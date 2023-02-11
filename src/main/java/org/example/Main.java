6ea93ab32d3a63b76952d8a389b816d807a97627
khanian <khanylee@gmail.com>
2023-02-11 11:34:36 +0900

    add first http test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 // get /calculate?operand1=11&operator=*&operand2=55
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        //System.out.println("Hello world!");
         new CustomWebApplicationServer(8080).start();
     }
 }

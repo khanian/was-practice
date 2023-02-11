0ad17d05e79c28e3737a52b0d4a39c5e980ee7c6
khanian <khanylee@gmail.com>
2023-02-11 11:58:27 +0900

    add first http test 2

--- src/main/java/org/example/RequestLine.java
+++ src/main/java/org/example/RequestLine.java
@@ -1,7 +1,29 @@
 package org.example;
 
 public class RequestLine {
-    public RequestLine(String requestLine) {
+    private final String method;    // GET
+    private final String urlPath;   // /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
+    private String queryString;
 
+    public RequestLine(String method, String urlPath, String queryString) {
+        this.method = method;
+        this.urlPath = urlPath;
+        this.queryString = queryString;
+    }
+
+    /**
+     * GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
+     * HttpMethod, path, queryString
+     */
+    public RequestLine(String requestLine) {
+        String[] tokens = requestLine.split(" ");
+        this.method = tokens[0];
+        //this.urlPath = tokens[1];
+        String[] urlPathTokens = tokens[1].split("\\?");
+        this.urlPath = urlPathTokens[0];
+
+        if(urlPathTokens.length == 2) {
+            this.queryString = urlPathTokens[1];
+        }
     }
 }

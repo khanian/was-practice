ff9f30095b8c78e4908f7a5da0026beb355e5b50
khanian <khanylee@gmail.com>
2023-02-11 11:13:48 +0900

    add first test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.io.IOException;
+
+// get /calculate?operand1=11&operator=*&operand2=55
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        //System.out.println("Hello world!");
+        new CustomWebApplicationServer(8080).start();
     }
 }

29e22f7211d7bd1d7b1d02f7bdeed40650c2bd44
khanian <khanylee@gmail.com>
2023-02-12 10:14:48 +0900

    add new thread http test response string line

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -1,14 +1,11 @@
 package org.example;
 
-import org.example.calculator.Calculator;
-import org.example.calculator.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 public class CustomWebApplicationServer {
     private final int port;
@@ -30,17 +27,22 @@ public class CustomWebApplicationServer {
                 logger.info("[CustomWebApplicationServer] client connected");
 
                 /**
+                 * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
+                 */
+                new Thread(new ClientRequestHandler(clientSocket)).start();
+
+                /**
                  * Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
                  */
-                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+                /*try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
                     BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
                     DataOutputStream dos = new DataOutputStream(out);
 
                     // show protocol
-                    /*String line;
+                    *//*String line;
                     while ((line = br.readLine()) != "") {
                         System.out.println(line);
-                    }*/
+                    }*//*
 
                     HttpRequest httpRequest = new HttpRequest(br);
 
@@ -60,7 +62,7 @@ public class CustomWebApplicationServer {
                         response.response200Header("application/json", body.length);
                         response.responseBody(body);
                     }
-                }
+                }*/
             }
         }
     }

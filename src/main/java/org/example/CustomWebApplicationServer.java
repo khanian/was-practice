ae31601a875a95268cc21cb8b24ea9db4b86df5d
khanian <khanylee@gmail.com>
2023-02-12 09:33:54 +0900

    add stream http test querystring line

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.calculator.Calculator;
+import org.example.calculator.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,9 +36,23 @@ public class CustomWebApplicationServer {
                     BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
                     DataOutputStream dos = new DataOutputStream(out);
 
-                    String line;
+                    // show protocol
+                    /*String line;
                     while ((line = br.readLine()) != "") {
                         System.out.println(line);
+                    }*/
+
+                    HttpRequest httpRequest = new HttpRequest(br);
+
+                    // GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
+                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
+                        QueryStrings queryStrings = httpRequest.getQueryStrings();
+
+                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
+                        String operator = queryStrings.getValue("operator");
+                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
+
+                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
                     }
                 }
             }
--- src/main/java/org/example/RequestLine.java
+++ src/main/java/org/example/RequestLine.java
@@ -5,12 +5,12 @@ import java.util.Objects;
 public class RequestLine {
     private final String method;    // GET
     private final String urlPath;   // /calculate?
-    private String queryString;     // operand1=11&operator=*&operand2=55
+    private QueryStrings queryStrings;     // operand1=11&operator=*&operand2=55
 
-    public RequestLine(String method, String urlPath, String queryString) {
+    public RequestLine(String method, String urlPath, String queryStrings) {
         this.method = method;
         this.urlPath = urlPath;
-        this.queryString = queryString;
+        this.queryStrings = new QueryStrings(queryStrings);
     }
 
     /**
@@ -20,25 +20,38 @@ public class RequestLine {
     public RequestLine(String requestLine) {
         String[] tokens = requestLine.split(" ");
         this.method = tokens[0];
-        //this.urlPath = tokens[1];
+        //this.urlPath = tokens[1]; // all of query string
         String[] urlPathTokens = tokens[1].split("\\?");
         this.urlPath = urlPathTokens[0];
 
         if(urlPathTokens.length == 2) {
-            this.queryString = urlPathTokens[1];
+            this.queryStrings = new QueryStrings(urlPathTokens[1]);
         }
     }
 
+    public boolean isGetRequest() {
+        return "GET".equals(this.method);
+    }
+
+    public boolean matchPath(String requestPath) {
+        return this.urlPath.equals(requestPath);
+    }
+
+    public QueryStrings getQueryStrings() {
+        return this.queryStrings;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RequestLine that = (RequestLine) o;
-        return Objects.equals(method, that.method) && Objects.equals(urlPath, that.urlPath) && Objects.equals(queryString, that.queryString);
+        return Objects.equals(method, that.method) && Objects.equals(urlPath, that.urlPath) && Objects.equals(queryStrings, that.queryStrings);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(method, urlPath, queryString);
+        return Objects.hash(method, urlPath, queryStrings);
     }
+
+
 }

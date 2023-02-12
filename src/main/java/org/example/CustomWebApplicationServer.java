cae5dbf2714da128014a39b68806b468fd40b2d7
khanian <khanylee@gmail.com>
2023-02-12 09:45:22 +0900

    add stream http test response string line

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -52,7 +52,13 @@ public class CustomWebApplicationServer {
                         String operator = queryStrings.getValue("operator");
                         int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
 
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+                        int result = Calculator.calculate(new PositiveNumber(operand1)
+                                , operator, new PositiveNumber(operand2));
+                        byte[] body = String.valueOf(result).getBytes();
+
+                        HttpResponse response = new HttpResponse(dos);
+                        response.response200Header("application/json", body.length);
+                        response.responseBody(body);
                     }
                 }
             }
